2b5934e8a86b07cffb21b92752c01cdfe0ff2c0a
Brent Zey <brentmzey4795@gmail.com>
2021-02-21 11:06:42 -0600

    first commit

---
+++ worker/index.js
@@ -0,0 +1,25 @@
+const keys = require("./keys");
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();
+
+let fibCache = {} 
+const fib = (index) => {
+    if (fibCache.index) {return fibCache.index};
+    if (index < 2) {value = 1};
+    if (index >=2) {
+        value = fib(index - 1) + fib(index - 2)
+    };
+    fibCache.index = value;
+    return value;
+}
+
+sub.on("message", (channel, message) => {
+    redisClient.hset("values", message, fib(parseInt(message)));
+});
+sub.subscribe("insert");
